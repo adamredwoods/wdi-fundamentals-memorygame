3bf91b8861f81a7d94acc77e52c9dad7cefcc870
adamredwoods <adam@piettes.com>
2017-11-18 18:53:37 -0800

    added card objects

--- js/main.js
+++ js/main.js
@@ -2,10 +2,26 @@
 
 
 var cards = [
-   "queen",
-   "queen",
-   "king",
-   "king"
+   {
+      rank: "queen",
+      suit: "hearts",
+      cardImage: "images/queen-of-hearts.png"
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png"
+   },
+   {
+      rank: "king",
+      suit: "hearts",
+      cardImage: "images/king-of-hearts.png"
+   },
+   {
+      rank: "king",
+      suit: "diamonds",
+      cardImage: "images/king-of-diamonds.png"
+   }
 ];
 
 var cardsInPlay = [];
@@ -15,9 +31,10 @@ var cardsInPlay = [];
 var flipCard = function( cardId ) {
 
 
-   cardsInPlay.push(cards[cardId]);
+   cardsInPlay.push(cards[cardId].rank);
 
-   console.log("User flipped "+cards[cardId]);
+   console.log("User flipped "+cards[cardId].rank);
+   console.log(cards[cardId].cardImage);
 };
 
 var checkForMatch = function() {
